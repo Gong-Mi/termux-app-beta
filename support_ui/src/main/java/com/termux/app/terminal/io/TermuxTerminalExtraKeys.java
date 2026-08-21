df16184e36a869c3ba3fd4e9c478f39c14251959
Gong-Mi <62661921+Gong-Mi@users.noreply.github.com>
2026-08-21 11:30:11 +0800

    fix(TermuxTerminalExtraKeys): add missing TerminalSession import

--- support_ui/src/main/java/com/termux/app/terminal/io/TermuxTerminalExtraKeys.java
+++ support_ui/src/main/java/com/termux/app/terminal/io/TermuxTerminalExtraKeys.java
@@ -16,6 +16,7 @@ import com.termux.shared.termux.extrakeys.ExtraKeysInfo;
 import com.termux.shared.termux.settings.properties.TermuxPropertyConstants;
 import com.termux.shared.termux.settings.properties.TermuxSharedProperties;
 import com.termux.shared.termux.terminal.io.TerminalExtraKeys;
+import com.termux.terminal.TerminalSession;
 import com.termux.view.TerminalView;
 
 import org.json.JSONException;
